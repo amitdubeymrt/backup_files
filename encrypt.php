279d9871138e6f44069aa14a6f57e1694399a530
harpreetspec <harpreet.kaur@spectra.co>
2020-10-08 14:21:30 +0530

    first commit

---
+++ encrypt.php
@@ -0,0 +1,36 @@
+<?php
+$id= "Hello World!";
+$encrypted = encrypt($id, "RjvnxcSq");
+echo $encrypted ;
+
+function encrypt($string, $key) {
+  $result = '';
+  for($i=0; $i<strlen($string); $i++) {
+    $char = substr($string, $i, 1);
+    $keychar = substr($key, ($i % strlen($key))-1, 1);
+    $char = chr(ord($char)+ord($keychar));
+    $result.=$char;
+  }
+
+  return base64_encode($result);
+}
+
+echo "\n";
+
+$decrypted = decrypt($encrypted, "RjvnxcSq");
+echo $decrypted ;
+
+function decrypt($string, $key) {
+  $result = '';
+  $string = base64_decode($string);
+
+  for($i=0; $i<strlen($string); $i++) {
+    $char = substr($string, $i, 1);
+    $keychar = substr($key, ($i % strlen($key))-1, 1);
+    $char = chr(ord($char)-ord($keychar));
+    $result.=$char;
+  }
+
+  return $result;
+}
+?>
---
+++ jquery_encode.php
@@ -0,0 +1,3 @@
+$.base64.encode("this is a test");
+$.base64.decode("dGhpcyBpcyBhIHRlc3Q=");
+
